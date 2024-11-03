3b0ca2f1b005d20d71c2467cbc0a5b81b41ee843
sebastt32 <sebastian14.mg@gmail.com>
2024-11-02 22:53:00 -0500

    sedff

--- resources/views/permissions/permissions.blade.php
+++ resources/views/permissions/permissions.blade.php
@@ -15,6 +15,7 @@
 </body>
 </html>
 
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -55,5 +56,6 @@
             </div>
         </div>
     </div>
+</div>  
 
 @endsection
